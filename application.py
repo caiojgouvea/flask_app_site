2b7a9161798192018eab7022832efedd851f637a
Caio Gouvea <caiojuniogouvea@gmail.com>
2024-11-22 11:03:22 -0300

    add bootstrap

--- application.py
+++ application.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, send_from_directory
 
+
 app = Flask(__name__)
+
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(app.root_path + '/static', 'favicon.png')
+    return send_from_directory(app.root_path + '/static/imgs', 'favicon.png')
+
 
 @app.route('/index')
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
